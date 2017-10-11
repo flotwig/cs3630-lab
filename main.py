a810f2fccd1f15738f6996678491b07bc53a790c
Zach Bloomquist <zbloomq@live.com>
2017-10-11 18:32:04 -0400

    FindARCube now pretty good. Other tiny enhancements

--- main.py
+++ main.py
@@ -19,6 +19,13 @@ import pdb
 PINK_LOWER = np.array(np.array([168, 150, 141]).round(), np.uint8)
 PINK_UPPER = np.array(np.array([180, 224, 255]).round(), np.uint8)
 
+COLOR_PINK = cozmo.lights.Color(rgb=(255, 0, 255))
+COLOR_LIME = cozmo.lights.Color(rgb=(0, 255, 0))
+
+LIGHT_EXCITED = cozmo.lights.Light(on_color=COLOR_PINK, off_color=COLOR_LIME, on_period_ms=100, off_period_ms=100,
+                                   transition_on_period_ms=50, transition_off_period_ms=50)
+LIGHT_CALM = cozmo.lights.Light(on_color=cozmo.lights.blue, off_color=cozmo.lights.white, on_period_ms=300,
+                                off_period_ms=300, transition_on_period_ms=1000, transition_off_period_ms=1000)
 thresholdWindowName = "Adjust Thresholds"
 lowerThreshold = PINK_LOWER
 upperThreshold = PINK_UPPER
@@ -49,32 +56,32 @@ def run(robot: cozmo.robot.Robot):
     state = FindARCube
     while state:
         event = robot.world.wait_for(
-            cozmo.camera.EvtNewRawCameraImage, timeout=30)  #get camera image
+            cozmo.camera.EvtNewRawCameraImage, timeout=30)  # get camera image
         if event.image is not None:
             image = cv2.cvtColor(np.asarray(event.image), cv2.COLOR_BGR2RGB)
 
-        if last_state != state:  #state change
+        if last_state != state:  # state change
             face = cozmo.oled_face.convert_image_to_screen_data(
                 generate_face(state))
-            robot.display_oled_face_image(face, 30000, in_parallel=True)
+            display = robot.display_oled_face_image(face, 30000, in_parallel=True)
+
             if last_state != None:
                 print("Leaving state: " + state.name)
             print("Entering state: " + state.name)
             robot.say_text(
-                "Entering " + state.name,
+                "Entering " + state.phonetic_name,
                 use_cozmo_voice=False,
                 in_parallel=True)
 
         last_state = state
         state = state.act(robot)
 
-
+# generates an image with state's name centered
 def generate_face(state):
-    # make a blank image for the text, initialized to opaque black
-    text_image = Image.new('RGBA', cozmo.oled_face.dimensions(), (0, 0, 0,
-                                                                  255))
+    dim = cozmo.oled_face.dimensions()
+    text_image = Image.new('RGBA', dim, (0, 0, 0, 255))
     dc = ImageDraw.Draw(text_image)
-    dc.text((0, 0), state.name, fill=(255, 255, 255, 255))
+    dc.text((dim[0]/2 - (len(state.name) * 3), dim[1]/2), state.name, fill=(255, 255, 255, 255))
     return text_image
 
     
@@ -106,61 +113,45 @@ def adjustThresholds():
 
 
 class FindARCube:
-    name = "Find Ay R Cube"
+    phonetic_name = "Find Ay R Cube"
+    name = "Find AR Cube"
 
     def act(robot: cozmo.robot.Robot):
         adjustThresholds()
-        rotation_speed = 10 #radians /s
-        wheel_radius = 13 #mm
-        max_fwd_speed = 15 #radians /s
-        stop_distance = 80  # mm
-        good_enough_angle = 10 #degrees
-        near_cube = False
-        robot.drive_wheels(-1 * rotation_speed, rotation_speed) #begin rotating
-        cube = robot.world.wait_for_observed_light_cube()
-        cube.set_lights(cozmo.lights.green_light)
-        while not near_cube:
-            try:
-                cube = robot.world.wait_for_observed_light_cube(timeout=5)
-            except:
-                robot.drive_wheels(-1 * rotation_speed, rotation_speed)
-                continue
-            cozmo_pos = robot.pose.position
-            cube_pos = cube.pose.position
-            angle_to_go = np.degrees(np.arctan(abs((cube_pos.y - cozmo_pos.y) / (cube_pos.x - cozmo_pos.x)))) - robot.pose_angle.degrees
-            distance_to_go = np.sqrt((cube_pos.x - cozmo_pos.x)**2 + (cube_pos.y - cozmo_pos.y)**2)
-            print("cube", cube_pos)
-            print("cozmo", cozmo_pos)
-            wheel_fwd_speed = 0
-            if abs(angle_to_go) <= good_enough_angle: # first rotate to face...
-                wheel_rot_speed = 0
-                #robot.stop_all_motors()
-                if distance_to_go > stop_distance: # then move to...
-                    wheel_fwd_speed = min(rotation_speed * 10, distance_to_go / (wheel_radius / 2))
-            else:
-                wheel_rot_speed = np.sign(angle_to_go) * min(abs(angle_to_go), rotation_speed)
-            if distance_to_go <= stop_distance and abs(angle_to_go) <= good_enough_angle:
-                robot.stop_all_motors()
-                near_cube = True
-                continue
-            else:
-                l_speed = wheel_fwd_speed + wheel_rot_speed
-                r_speed = wheel_fwd_speed - wheel_rot_speed
-                print(angle_to_go, distance_to_go, wheel_fwd_speed, wheel_rot_speed, l_speed, r_speed)
-                robot.drive_wheels(l_speed, r_speed)
-                time.sleep(.1)
-        cube.set_lights(cozmo.lights.blue_light)
+        rotation_speed = 8
+        stop_distance = 70
+        stop_angle = 5
+        while True:
+            robot.drive_wheels(-1 * rotation_speed, rotation_speed)  # begin rotating
+            cube = robot.world.wait_for_observed_light_cube()
+            cube.set_lights(LIGHT_EXCITED)
+            really_stop(robot)
+            go_to_pose = robot.go_to_pose(cube.pose, in_parallel=True, relative_to_robot=False)
+            while not (go_to_pose.is_completed or go_to_pose.is_aborting):
+                difference = go_to_pose.pose - robot.pose
+                distance = np.sqrt(difference.position.x ** 2 + difference.position.y ** 2)
+                angle_diff = abs(difference.rotation.angle_z.degrees)
+                print(go_to_pose.pose.is_comparable(robot.pose), distance, angle_diff)
+                if distance <= stop_distance and angle_diff <= stop_angle:
+                    go_to_pose.abort()
+                    really_stop(robot)
+                time.sleep(.02)
+            go_to_pose.wait_for_completed()
+            print(go_to_pose.result, go_to_pose.has_succeeded, go_to_pose.has_failed, go_to_pose.failure_reason)
+            if go_to_pose.result == cozmo.action.ActionResults.CANCELLED_WHILE_RUNNING:
+                break
+        cube.set_lights(LIGHT_CALM)
         return FindColorCubeLeft
 
 class FindColorCubeLeft:
-    name = "Find Color Cube Left"
+    phonetic_name = name = "Find Color Cube Left"
     
     def act(robot: cozmo.robot.Robot):
         return findColorCube(robot, -1)
         
 
 class FindColorCubeRight:
-    name = "Find Color Cube Right"
+    phonetic_name = name = "Find Color Cube Right"
     
     def act(robot: cozmo.robot.Robot):
         return findColorCube(robot, 1)
@@ -182,7 +173,7 @@ def findColorCube(robot: cozmo.robot.Robot, direction):
 
 
 class MoveToColorCube:
-    name = "Move to Color Cube"
+    phonetic_name = name = "Move to Color Cube"
     
     def act(robot: cozmo.robot.Robot):
         delta = 0
@@ -217,7 +208,7 @@ class MoveToColorCube:
                         
                         
 class Stop:
-    name = "Stop"
+    phonetic_name = name = "Stop"
     
     def act(robot: cozmo.robot.Robot):
         delta = 0
@@ -248,7 +239,12 @@ class Stop:
 # x displacement of cube blob from center of screen (as a percentage of IMAGE_WIDTH / 2)
 def cubeDelta(cube):
     return (cube[0] - (IMAGE_WIDTH / 2)) / (IMAGE_WIDTH / 2)
-                        
+
+# for some reason stop_all_motors leaves cozmo wiggling, this is to circumvent that
+def really_stop(robot: cozmo.robot.Robot):
+    robot.stop_all_motors()
+    robot.drive_wheel_motors(0, 0)
+
 class RunningAverage:
     def __init__(self, size):
         self.list = list()
