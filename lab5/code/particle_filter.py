4821a5ce10e9abac455ea27dd1fe92eb6a0b8384
Zach Bloomquist <zbloomq@live.com>
2017-11-13 14:13:00 -0500

    Implemented sample_motion_model

--- lab5/code/particle_filter.py
+++ lab5/code/particle_filter.py
@@ -4,6 +4,8 @@ from utils import *
 from setting import *
 import numpy as np
 
+ALPHA_1, ALPHA_2, ALPHA_3, ALPHA_4 = [0, 0, 0, 0]
+
 # ------------------------------------------------------------------------
 def motion_update(particles, odom):
     """ Particle filter motion update
@@ -17,12 +19,19 @@ def motion_update(particles, odom):
         Returns: the list of particle represents belief \tilde{p}(x_{t} | u_{t})
                 after motion update
     """
-    odom = add_odometry_noise(odom, ODOM_HEAD_SIGMA, ODOM_TRANS_SIGMA)
-    delta_x, delta_y, delta_heading = np.subtract(odom[1], odom[0])
+    odom = add_odometry_noise(odom, ODOM_HEAD_SIGMA, ODOM_TRANS_SIGMA) # necessary?
+    prev_odom, cur_odom = odom
+    diff_odom = np.subtract(cur_odom, prev_odom)
+    delta_rot1 = diff_heading_deg(np.arctan2(diff_odom[1], diff_odom[0]), prev_odom[2])
+    delta_trans = np.sqrt(diff_odom[0]**2 + diff_odom[1]**2)
+    delta_rot2 = diff_odom[2] - delta_rot1
     for particle in particles:
-        particle.x += delta_x
-        particle.y += delta_y
-        particle.h += delta_heading
+        pdelta_rot1 = delta_rot1 - np.random.normal(loc=0, scale=abs(ALPHA_1 * delta_rot1 + ALPHA_2 * delta_trans))
+        pdelta_trans = delta_trans - np.random.normal(loc=0, scale=abs(ALPHA_3 * delta_trans + ALPHA_4 * (delta_rot1 + delta_rot2)))
+        pdelta_rot2 = delta_rot2 - np.random.normal(loc=0, scale=abs(ALPHA_1 * delta_rot2 + ALPHA_2 * delta_trans))
+        particle.x += pdelta_trans * np.cos(particle.h + pdelta_rot1)
+        particle.y += pdelta_trans * np.sin(particle.h + pdelta_rot1)
+        particle.h += pdelta_rot1 + pdelta_rot2
     return particles
 
 # ------------------------------------------------------------------------
@@ -73,6 +82,6 @@ def measurement_update(particles, measured_marker_list, grid):
             prob *= math.e ** (0 - ((dist**2)/(2*MARKER_TRANS_SIGMA**2) + (angle**2)/(2*MARKER_ROT_SIGMA**2)))
         probabilities.append(prob)
     # normalize probabilities and choose particles
-    probabilities = np.divide(probabilities, np.sum(probabilities))
+    probabilities = np.divide(probabilities, [np.sum(probabilities)])
     measured_particles = np.random.choice(particles, p=probabilities, size=5000)
     return measured_particles
