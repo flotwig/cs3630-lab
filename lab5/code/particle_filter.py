af6d6165f25b8b82be2487c1e68e0444d06d0bbc
Zach Bloomquist <zbloomq@live.com>
2017-11-13 18:42:16 -0500

    Reasonable alphas, low-probability filter, use degrees

--- lab5/code/particle_filter.py
+++ lab5/code/particle_filter.py
@@ -4,7 +4,8 @@ from utils import *
 from setting import *
 import numpy as np
 
-ALPHA_1, ALPHA_2, ALPHA_3, ALPHA_4 = [0.1, 0.1, 0.1, 0.1]
+ALPHA_1, ALPHA_2, ALPHA_3, ALPHA_4 = [0.01, 0.01, 0.01, 0.01]
+MIN_PROBABILITY = .1  # particles with p < this will be removed
 
 # ------------------------------------------------------------------------
 def motion_update(particles, odom):
@@ -21,17 +22,16 @@ def motion_update(particles, odom):
     """
     #odom = add_odometry_noise(odom, ODOM_HEAD_SIGMA, ODOM_TRANS_SIGMA) # necessary?
     prev_odom, cur_odom = odom
-    diff_odom = np.subtract(cur_odom, prev_odom)
-    delta_rot1 = diff_heading_deg(np.arctan2(diff_odom[1], diff_odom[0]), prev_odom[2])
-    delta_trans = np.sqrt(diff_odom[0]**2 + diff_odom[1]**2)
-    delta_rot2 = diff_odom[2] - delta_rot1
+    delta_rot1 = np.degrees(np.arctan2(cur_odom[1] - prev_odom[1], cur_odom[0] - prev_odom[0])) - prev_odom[2]
+    delta_trans = np.sqrt((cur_odom[1] - prev_odom[1])**2 + (cur_odom[0] - prev_odom[0])**2)
+    delta_rot2 = cur_odom[2] - prev_odom[2] - delta_rot1
     for particle in particles:
         pdelta_rot1 = delta_rot1 - np.random.normal(loc=0, scale=abs(ALPHA_1 * delta_rot1 + ALPHA_2 * delta_trans))
         pdelta_trans = delta_trans - np.random.normal(loc=0, scale=abs(ALPHA_3 * delta_trans + ALPHA_4 * (delta_rot1 + delta_rot2)))
         pdelta_rot2 = delta_rot2 - np.random.normal(loc=0, scale=abs(ALPHA_1 * delta_rot2 + ALPHA_2 * delta_trans))
-        particle.x += pdelta_trans * np.cos(particle.h + pdelta_rot1)
-        particle.y += pdelta_trans * np.sin(particle.h + pdelta_rot1)
-        particle.h += pdelta_rot1 + pdelta_rot2
+        particle.x += pdelta_trans * np.cos(np.radians(particle.h + pdelta_rot1))
+        particle.y += pdelta_trans * np.sin(np.radians(particle.h + pdelta_rot1))
+        particle.h += proj_angle_deg(pdelta_rot1 + pdelta_rot2)
     return particles
 
 # ------------------------------------------------------------------------
@@ -82,8 +82,12 @@ def measurement_update(particles, measured_marker_list, grid):
             dist = grid_distance(measured_marker[0], measured_marker[1], min_marker[0], min_marker[1])
             angle = diff_heading_deg(measured_marker[2], min_marker[2])
             prob *= math.e ** (0 - ((dist**2)/(2*(MARKER_TRANS_SIGMA**2)) + (angle**2)/(2*(MARKER_ROT_SIGMA**2))))
+        if prob < MIN_PROBABILITY:
+            prob = 0
         probabilities.append(prob)
     # normalize probabilities and choose particles
-    probabilities = np.divide(probabilities, np.sum(probabilities))
-    measured_particles = np.random.choice(particles, p=probabilities, size=5000)
-    return measured_particles
+    probabilities = np.divide(probabilities, [np.sum(probabilities)])
+    measured_particles = np.random.choice(particles, p=probabilities, size=4700)
+    # let's aim for 5000 particles
+    new_particles = Particle.create_random(5000 - len(measured_particles), grid)
+    return np.concatenate([measured_particles, new_particles])
