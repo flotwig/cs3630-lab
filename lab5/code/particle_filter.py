5771c4386e5b3441ef3a4351e18add37cfb5f79b
Zach Bloomquist <zbloomq@live.com>
2017-11-13 23:32:57 -0500

    Convergy Convergyface

--- lab5/code/particle_filter.py
+++ lab5/code/particle_filter.py
@@ -5,12 +5,12 @@ from setting import *
 from copy import copy
 import numpy as np
 
-ALPHA_1, ALPHA_2, ALPHA_3, ALPHA_4 = [0.01, 0.01, 0.01, 0.01]
-MIN_PROBABILITY = .05  # particles with p < this will be removed
-
+ALPHA_1, ALPHA_2, ALPHA_3, ALPHA_4 = [.1, .1, .05, .05]
+MIN_PROBABILITY = 0.2 # particles with p < this will be removed
+NEW_PARTICLE_WEIGHT = 5000  # new random particle weight = (max_prob)/NEW_PARTICLE_WEIGHT
 # ------------------------------------------------------------------------
 def motion_update(particles, odom):
-    """ Particle filter motion update
+    """ Particle filter motion upda
 
         Arguments:
         particles -- input list of particle represents belief p(x_{t-1} | u_{t-1})
@@ -29,7 +29,7 @@ def motion_update(particles, odom):
     # rot1: angle robot needs to turn before moving
     delta_rot1 = np.degrees(np.arctan2(cur_odom[1] - prev_odom[1], cur_odom[0] - prev_odom[0])) - prev_odom[2]
     # trans: distance robot should traverse forward after rotating
-    delta_trans = np.sqrt((cur_odom[1] - prev_odom[1])**2 + (cur_odom[0] - prev_odom[0])**2)
+    delta_trans = np.sqrt((prev_odom[0] - cur_odom[0])**2 + (prev_odom[1] - cur_odom[1])**2)
     # rot2: angle robot should turn when reaching destination to match heading
     delta_rot2 = cur_odom[2] - prev_odom[2] - delta_rot1
     for i, particle in enumerate(particles):
@@ -38,7 +38,7 @@ def motion_update(particles, odom):
         pdelta_trans = delta_trans - random.gauss(0.0, ALPHA_3 * delta_trans + ALPHA_4 * (delta_rot1 + delta_rot2))**2
         pdelta_rot2 = delta_rot2 - random.gauss(0.0, ALPHA_1 * delta_rot2 + ALPHA_2 * delta_trans)**2
         particle.move(pdelta_rot1, pdelta_trans, pdelta_rot2)
-        particles[i] = Particle(particle.x, particle.y, particle.h)  # need a new object, i think?
+        particles[i] = Particle(particle.x, particle.y, heading=particle.h)  # need a new object, i think?
     return particles
 
 # ------------------------------------------------------------------------
@@ -89,15 +89,22 @@ def measurement_update(particles, measured_marker_list, grid):
             prob = 0.0
         for marker in found_markers:
             measured_marker, min_marker, dist = marker
-            angle = diff_heading_deg(measured_marker[2], proj_angle_deg(min_marker[2]))
+            angle = diff_heading_deg(measured_marker[2], min_marker[2])
             prob *= math.exp(0 - ((dist**2)/(2*(MARKER_TRANS_SIGMA**2)) + (angle**2)/(2*(MARKER_ROT_SIGMA**2))))
         probabilities.append(prob)
     # replace improbables with new particles
-    for i, p in enumerate(probabilities):
+    max_prob = max(probabilities)
+    for (i, p) in enumerate(probabilities):
         if p < MIN_PROBABILITY:
-            probabilities[i] = 0
+            probabilities[i] = max_prob/NEW_PARTICLE_WEIGHT
             particles[i] = Particle.create_random(1, grid)[0]
     # normalize probabilities and choose particles
     probabilities = np.divide(probabilities, [np.sum(probabilities)])
-    measured_particles = np.random.choice(particles, p=probabilities, size=4975)
+    measured_particles = np.random.choice(particles, p=probabilities, size=5000)
+    for i, particle in enumerate(measured_particles):
+        measured_particles[i] = Particle(particle.x + random.gauss(0, MARKER_TRANS_SIGMA**2), particle.y + random.gauss(0, MARKER_TRANS_SIGMA**2), particle.h + random.gauss(0, 9))
+    if len(measured_particles) == 0:
+        return particles
     return measured_particles
+
+
