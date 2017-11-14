9642051504cb105eadfb39ab943965bb82be7b8d
Zach Bloomquist <zbloomq@live.com>
2017-11-13 19:39:55 -0500

    code cleanup

--- lab5/code/particle_filter.py
+++ lab5/code/particle_filter.py
@@ -5,7 +5,7 @@ from setting import *
 import numpy as np
 
 ALPHA_1, ALPHA_2, ALPHA_3, ALPHA_4 = [0.01, 0.01, 0.01, 0.01]
-MIN_PROBABILITY = .1  # particles with p < this will be removed
+MIN_PROBABILITY = .05  # particles with p < this will be removed
 
 # ------------------------------------------------------------------------
 def motion_update(particles, odom):
@@ -20,18 +20,26 @@ def motion_update(particles, odom):
         Returns: the list of particle represents belief \tilde{p}(x_{t} | u_{t})
                 after motion update
     """
+    #algorithm https://i.imgur.com/rJ6CUmu.png
     #odom = add_odometry_noise(odom, ODOM_HEAD_SIGMA, ODOM_TRANS_SIGMA) # necessary?
     prev_odom, cur_odom = odom
+    if np.array_equal(prev_odom, cur_odom):
+        return particles
+    # rot1: angle robot needs to turn before moving
     delta_rot1 = np.degrees(np.arctan2(cur_odom[1] - prev_odom[1], cur_odom[0] - prev_odom[0])) - prev_odom[2]
+    # trans: distance robot should traverse forward after rotating
     delta_trans = np.sqrt((cur_odom[1] - prev_odom[1])**2 + (cur_odom[0] - prev_odom[0])**2)
+    # rot2: angle robot should turn when reaching destination to match heading
     delta_rot2 = cur_odom[2] - prev_odom[2] - delta_rot1
     for particle in particles:
-        pdelta_rot1 = delta_rot1 - np.random.normal(loc=0, scale=abs(ALPHA_1 * delta_rot1 + ALPHA_2 * delta_trans))
-        pdelta_trans = delta_trans - np.random.normal(loc=0, scale=abs(ALPHA_3 * delta_trans + ALPHA_4 * (delta_rot1 + delta_rot2)))
-        pdelta_rot2 = delta_rot2 - np.random.normal(loc=0, scale=abs(ALPHA_1 * delta_rot2 + ALPHA_2 * delta_trans))
+        # add gaussian noise to deltas
+        pdelta_rot1 = delta_rot1 - random.gauss(0.0, ALPHA_1 * delta_rot1 + ALPHA_2 * delta_trans)
+        pdelta_trans = delta_trans - random.gauss(0.0, ALPHA_3 * delta_trans + ALPHA_4 * (delta_rot1 + delta_rot2))
+        pdelta_rot2 = delta_rot2 - random.gauss(0.0, ALPHA_1 * delta_rot2 + ALPHA_2 * delta_trans)
+        # extrapolate x, y, and h from deltas
         particle.x += pdelta_trans * np.cos(np.radians(particle.h + pdelta_rot1))
         particle.y += pdelta_trans * np.sin(np.radians(particle.h + pdelta_rot1))
-        particle.h += proj_angle_deg(pdelta_rot1 + pdelta_rot2)
+        particle.h = particle.h + pdelta_rot1 + pdelta_rot2
     return particles
 
 # ------------------------------------------------------------------------
@@ -54,16 +62,17 @@ def measurement_update(particles, measured_marker_list, grid):
     """
     measured_particles = []
     probabilities = []
-    for particle in particles:
-        # identify pairings between markers seen by particle & particles cozmo knows of
+    for j, particle in enumerate(particles):  # for each particle:
+        # Obtain the list of localization markers that a robot would see if it were really at this particle
         markers_visible_to_particle = particle.read_markers(grid)
-        # if the particle is outside map or in an obstacle, remove it
         if not grid.is_free(particle.x, particle.y):
+            # particles within an obstacle or outside the map should have a weight of 0
             probabilities.append(0)
             continue
+        # compare the list of markers obtained by simulating the particle’s field of view to the observed markers
         found_markers = []
         for measured_marker in measured_marker_list:
-            # find closest marker out of ones visible to particle
+            # try to match the markers by distance - nearest marker is probably it
             min_distance = -1
             min_marker = None
             i = None
@@ -74,20 +83,19 @@ def measurement_update(particles, measured_marker_list, grid):
                     min_marker = visible_marker
             if i is not None:
                 markers_visible_to_particle.pop(i)
-                found_markers.append([measured_marker, min_marker])
-        # calculate weight of this particle
+                found_markers.append([measured_marker, min_marker, min_distance])
+        # calculate weight of this particle based on how likely it is to see the markers the robot sees
         prob = 1.0
         for marker in found_markers:
-            measured_marker, min_marker = marker
-            dist = grid_distance(measured_marker[0], measured_marker[1], min_marker[0], min_marker[1])
+            measured_marker, min_marker, dist = marker
             angle = diff_heading_deg(measured_marker[2], min_marker[2])
             prob *= math.e ** (0 - ((dist**2)/(2*(MARKER_TRANS_SIGMA**2)) + (angle**2)/(2*(MARKER_ROT_SIGMA**2))))
         if prob < MIN_PROBABILITY:
-            prob = 0
+            prob = 0  # eliminating any particles with very low probabilities and replacing them with random samples
         probabilities.append(prob)
     # normalize probabilities and choose particles
     probabilities = np.divide(probabilities, [np.sum(probabilities)])
-    measured_particles = np.random.choice(particles, p=probabilities, size=4700)
-    # let's aim for 5000 particles
+    measured_particles = np.random.choice(particles, p=probabilities, size=4975)
+    # fill in the remaining particles with random ones - better way to do this? uniqify measured particles?
     new_particles = Particle.create_random(5000 - len(measured_particles), grid)
     return np.concatenate([measured_particles, new_particles])
