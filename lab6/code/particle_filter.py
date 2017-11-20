d49c090af766439e643c6bb9232fa46efd244d18
taylor1355 <hearntaylor@gmail.com>
2017-11-20 16:10:58 -0500

    copied last lab's code into particle_filter

--- lab6/code/particle_filter.py
+++ lab6/code/particle_filter.py
@@ -2,21 +2,55 @@ from grid import *
 from particle import Particle
 from utils import *
 from setting import *
+import numpy as np
+
+# ## Zach Bloomquist & Taylor Hearn
+# ## CS 3630 Lab 5
+
+ALPHA_1, ALPHA_2, ALPHA_3, ALPHA_4 = [.01, .01, .01, .01]
+MIN_PROBABILITY = 0.1  # particles with p < this will be removed. increase to find a fix faster, but lowers stability
+NEW_PARTICLE_WEIGHT = PARTICLE_COUNT * 4  # inverted - the higher, the less new random particles are chosen
+
 
 # ------------------------------------------------------------------------
 def motion_update(particles, odom):
-    """ Particle filter motion update
+    """ Particle filter motion upda
 
-        Arguments: 
+        Arguments:
         particles -- input list of particle represents belief p(x_{t-1} | u_{t-1})
                 before motion update
         odom -- noisy odometry measurement, a pair of robot pose, i.e. last time
-                step pose and current time step pose
+                step pose and current time step pose, both in form [x, y, theta]
 
         Returns: the list of particle represents belief \tilde{p}(x_{t} | u_{t})
                 after motion update
     """
+    # algorithm https://i.imgur.com/rJ6CUmu.png
+    prev_odom, cur_odom = odom
+    if np.array_equal(prev_odom, cur_odom):
+        return particles
+    # rot1: angle robot needs to turn before moving
+    delta_rot1 = diff_heading_deg(np.degrees(np.arctan2(cur_odom[1] - prev_odom[1],
+                                                        cur_odom[0] - prev_odom[0])), prev_odom[2])
+    # trans: distance robot should traverse forward after rotating
+    delta_trans = np.sqrt((prev_odom[0] - cur_odom[0])**2 + (prev_odom[1] - cur_odom[1])**2)
+    # rot2: angle robot should turn when reaching destination to match heading
+    delta_rot2 = proj_angle_deg(cur_odom[2] - prev_odom[2] - delta_rot1)
+    global moved
+    moved = delta_rot1 + delta_trans + delta_rot2 > 0.1
+    for i, particle in enumerate(particles):
+        # add gaussian noise to deltas
+        pdelta_rot1 = delta_rot1 - random.gauss(0.0, ALPHA_1 * delta_rot1 + ALPHA_2 * delta_trans)
+        pdelta_trans = delta_trans - random.gauss(0.0, ALPHA_3 * delta_trans + ALPHA_4 * (delta_rot1 + delta_rot2))
+        pdelta_rot2 = delta_rot2 - random.gauss(0.0, ALPHA_1 * delta_rot2 + ALPHA_2 * delta_trans)
+        particle.move(pdelta_rot1, pdelta_trans, pdelta_rot2)
+        particles[i] = Particle(particle.x, particle.y, heading=proj_angle_deg(particle.h))  # needs a new object
     return particles
+
+
+moved = True
+last_measured_marker_list = None
+
 
 # ------------------------------------------------------------------------
 def measurement_update(particles, measured_marker_list, grid):
@@ -36,5 +70,82 @@ def measurement_update(particles, measured_marker_list, grid):
         Returns: the list of particle represents belief p(x_{t} | u_{t})
                 after measurement update
     """
-    measured_particles = []
+    global last_measured_marker_list, moved
+    new_info = False
+    # no input, what we're doing is useless
+    if len(measured_marker_list) == 0:
+        return particles
+    if last_measured_marker_list is not None and len(last_measured_marker_list) == len(measured_marker_list) \
+            and not moved:
+        for i, marker in enumerate(measured_marker_list):
+            last_marker = last_measured_marker_list[i]
+            if marker[0] != last_marker[0] or marker[1] != last_marker[1] or marker[2] != last_marker[2]:
+                new_info = True
+                break
+        last_measured_marker_list = list(measured_marker_list)
+        if not new_info: return particles
+    probabilities = []
+    for j, particle in enumerate(particles):  # for each particle:
+        # Obtain the list of localization markers that a robot would see if it were really at this particle
+        markers_visible_to_particle = particle.read_markers(grid)
+        if not grid.is_free(particle.x, particle.y) or len(measured_marker_list) != len(markers_visible_to_particle):
+            # particles within an obstacle or outside the map should have a weight of 0
+            probabilities.append(0)
+            continue
+        # compare the list of markers obtained by simulating the particle’s field of view to the observed markers
+        found_markers = []
+        for measured_marker in measured_marker_list:
+            # try to match the markers by distance - nearest marker is probably it
+            min_distance = -1
+            min_marker = None
+            i = None
+            for i, visible_marker in enumerate(markers_visible_to_particle):
+                marker_distance = grid_distance(measured_marker[0], measured_marker[1],
+                                                visible_marker[0], visible_marker[1])
+                if marker_distance < min_distance or min_distance < 0:
+                    min_distance = marker_distance
+                    min_marker = visible_marker
+            if i is not None:
+                markers_visible_to_particle.pop(i)
+                found_markers.append([measured_marker, min_marker, min_distance])
+        # calculate weight of this particle based on how likely it is to see the markers the robot sees
+        prob = 1.0
+        if len(found_markers) == 0:
+            prob = 0.0
+        for marker in found_markers:
+            measured_marker, min_marker, dist = marker
+            angle = diff_heading_deg(measured_marker[2], min_marker[2])
+            prob *= math.exp(0 - ((dist**2)/(2*(MARKER_TRANS_SIGMA**2)) + (angle**2)/(2*(MARKER_ROT_SIGMA**2))))
+        probabilities.append(prob)
+    # replace improbable particles with new particles
+    max_prob = max(probabilities)
+    if max_prob < 1 / len(probabilities):
+        return particles
+    for (i, p) in enumerate(probabilities):
+        if p < MIN_PROBABILITY:
+            probabilities[i] = max_prob/NEW_PARTICLE_WEIGHT
+            particles[i] = Particle.create_random(1, grid)[0]
+    # normalize probabilities and choose particles
+    probabilities = np.divide(probabilities, [np.sum(probabilities)])
+    # sanity check on probs - if none of these particles are likely, return stale particle distribution
+    max_prob = max(probabilities)
+    if max_prob < 1 / len(probabilities):
+        return particles
+    measured_particles = np.random.choice(particles, p=probabilities, size=PARTICLE_COUNT)
+    # add some noise so that particles sampled multiple times won't be overlaid
+    for i, particle in enumerate(measured_particles):
+        h = proj_angle_deg(particle.h + random.gauss(0, MARKER_ROT_SIGMA))
+        x = particle.x + math.cos(math.radians(h)) * random.gauss(0, MARKER_TRANS_SIGMA)
+        y = particle.y + math.sin(math.radians(h)) * random.gauss(0, MARKER_TRANS_SIGMA)
+        measured_particles[i] = Particle(x, y, h)
+    if len(measured_particles) == 0:
+        return particles
+    # Alternate method for adding random particles - adds noise
+    # rand_particles = list()
+    # for x in range(1, grid.width, 1):
+    #     for y in range(1, grid.height, 1):
+    #         if grid.is_free(x, y):
+    #             rand_particles.append(Particle(x, y, random.uniform(0, 360)))
     return measured_particles
+
+
