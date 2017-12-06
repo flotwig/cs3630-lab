c3dc3a4b8bd59f70b3fe78b1da3db9bc25f100e4
taylor1355 <hearntaylor@gmail.com>
2017-12-06 18:12:12 -0500

    cleaned up pf code, made new particles' weight = actual probability

--- lab7/code/particle_filter.py
+++ lab7/code/particle_filter.py
@@ -9,7 +9,6 @@ import numpy as np
 
 ALPHA_1, ALPHA_2, ALPHA_3, ALPHA_4 = [.02, .02, .01, .01]
 MIN_PROBABILITY = 0.05  # particles with p < this will be removed. increase to find a fix faster, but lowers stability
-NEW_PARTICLE_WEIGHT = PARTICLE_COUNT * 16  # inverted - the higher, the less new random particles are chosen
 
 
 # ------------------------------------------------------------------------
@@ -53,6 +52,65 @@ last_measured_marker_list = None
 
 
 # ------------------------------------------------------------------------
+def new_info(measured_marker_list):
+    global last_measured_marker_list, moved
+    
+    if len(measured_marker_list) == 0:
+        return False
+        
+    if last_measured_marker_list is None or len(last_measured_marker_list) != len(measured_marker_list) or moved:
+        return True
+        
+    for i, marker in enumerate(measured_marker_list):
+        last_marker = last_measured_marker_list[i]
+        if marker[0] != last_marker[0] or marker[1] != last_marker[1] or marker[2] != last_marker[2]:
+            return True
+        
+    return False
+
+    
+def calculate_particle_probability(particle, measured_marker_list, grid):
+    # Obtain the list of localization markers that a robot would see if it were really at this particle
+    markers_visible_to_particle = particle.read_markers(grid)
+    if not grid.is_free(particle.x, particle.y) or len(measured_marker_list) != len(markers_visible_to_particle):
+        # particles within an obstacle or outside the map should have a weight of 0
+        return 0
+        
+    # compare the list of markers obtained by simulating the particle’s field of view to the observed markers
+    found_markers = []
+    for measured_marker in measured_marker_list:
+        # try to match the markers by distance - nearest marker is probably it
+        min_distance = -1
+        min_marker = None
+        i = None
+        for i, visible_marker in enumerate(markers_visible_to_particle):
+            marker_distance = grid_distance(measured_marker[0], measured_marker[1],
+                                            visible_marker[0], visible_marker[1])
+            if marker_distance < min_distance or min_distance < 0:
+                min_distance = marker_distance
+                min_marker = visible_marker
+        if i is not None:
+            markers_visible_to_particle.pop(i)
+            found_markers.append([measured_marker, min_marker, min_distance])
+            
+    # calculate weight of this particle based on how likely it is to see the markers the robot sees
+    prob = 1.0
+    if len(found_markers) == 0:
+        prob = 0.0
+    for marker in found_markers:
+        measured_marker, min_marker, dist = marker
+        angle = diff_heading_deg(measured_marker[2], min_marker[2])
+        prob *= math.exp(0 - ((dist**2)/(2*(MARKER_TRANS_SIGMA**2)) + (angle**2)/(2*(MARKER_ROT_SIGMA**2))))
+    return prob
+    
+
+def replace_unlikely_particles(particles, probabilities, measured_marker_list, grid):
+    for (i, prob) in enumerate(probabilities):
+        if prob < MIN_PROBABILITY:
+            particles[i] = Particle.create_random(1, grid)[0]
+            probabilities[i] = calculate_particle_probability(particles[i], measured_marker_list, grid)
+
+
 def measurement_update(particles, measured_marker_list, grid):
     """ Particle filter measurement update
 
@@ -71,60 +129,22 @@ def measurement_update(particles, measured_marker_list, grid):
                 after measurement update
     """
     global last_measured_marker_list, moved
-    new_info = False
+    
     # no input, what we're doing is useless
-    if len(measured_marker_list) == 0:
+    if not new_info(measured_marker_list):
         return particles
-    if last_measured_marker_list is not None and len(last_measured_marker_list) == len(measured_marker_list) \
-            and not moved:
-        for i, marker in enumerate(measured_marker_list):
-            last_marker = last_measured_marker_list[i]
-            if marker[0] != last_marker[0] or marker[1] != last_marker[1] or marker[2] != last_marker[2]:
-                new_info = True
-                break
-        last_measured_marker_list = list(measured_marker_list)
-        if not new_info: return particles
+    last_measured_marker_list = list(measured_marker_list)
+    
     probabilities = []
-    for j, particle in enumerate(particles):  # for each particle:
-        # Obtain the list of localization markers that a robot would see if it were really at this particle
-        markers_visible_to_particle = particle.read_markers(grid)
-        if not grid.is_free(particle.x, particle.y) or len(measured_marker_list) != len(markers_visible_to_particle):
-            # particles within an obstacle or outside the map should have a weight of 0
-            probabilities.append(0)
-            continue
-        # compare the list of markers obtained by simulating the particle’s field of view to the observed markers
-        found_markers = []
-        for measured_marker in measured_marker_list:
-            # try to match the markers by distance - nearest marker is probably it
-            min_distance = -1
-            min_marker = None
-            i = None
-            for i, visible_marker in enumerate(markers_visible_to_particle):
-                marker_distance = grid_distance(measured_marker[0], measured_marker[1],
-                                                visible_marker[0], visible_marker[1])
-                if marker_distance < min_distance or min_distance < 0:
-                    min_distance = marker_distance
-                    min_marker = visible_marker
-            if i is not None:
-                markers_visible_to_particle.pop(i)
-                found_markers.append([measured_marker, min_marker, min_distance])
-        # calculate weight of this particle based on how likely it is to see the markers the robot sees
-        prob = 1.0
-        if len(found_markers) == 0:
-            prob = 0.0
-        for marker in found_markers:
-            measured_marker, min_marker, dist = marker
-            angle = diff_heading_deg(measured_marker[2], min_marker[2])
-            prob *= math.exp(0 - ((dist**2)/(2*(MARKER_TRANS_SIGMA**2)) + (angle**2)/(2*(MARKER_ROT_SIGMA**2))))
-        probabilities.append(prob)
-    # replace improbable particles with new particles
+    for j, particle in enumerate(particles):
+        probabilities.append(calculate_particle_probability(particle, measured_marker_list, grid))
+        
     max_prob = max(probabilities)
     if max_prob < 1 / len(probabilities):
         return particles
-    for (i, p) in enumerate(probabilities):
-        if p < MIN_PROBABILITY:
-            probabilities[i] = max_prob/NEW_PARTICLE_WEIGHT
-            particles[i] = Particle.create_random(1, grid)[0]
+        
+    replace_unlikely_particles(particles, probabilities, measured_marker_list, grid)
+            
     # normalize probabilities and choose particles
     probabilities = np.divide(probabilities, [np.sum(probabilities)])
     # sanity check on probs - if none of these particles are likely, return stale particle distribution
@@ -132,13 +152,7 @@ def measurement_update(particles, measured_marker_list, grid):
     if max_prob < 1 / len(probabilities):
         return particles
     measured_particles = np.random.choice(particles, p=probabilities, size=PARTICLE_COUNT)
-    # add some noise so that particles sampled multiple times won't be overlaid
-    # lab6 - doesn't appear to be necessary in real world
-    #for i, particle in enumerate(measured_particles):
-    #    h = proj_angle_deg(particle.h + random.gauss(0, MARKER_ROT_SIGMA))
-    #    x = particle.x + math.cos(math.radians(h)) * random.gauss(0, MARKER_TRANS_SIGMA)
-    #    y = particle.y + math.sin(math.radians(h)) * random.gauss(0, MARKER_TRANS_SIGMA)
-    #    measured_particles[i] = Particle(x, y, h)
+
     if len(measured_particles) == 0:
         return particles
     return measured_particles
